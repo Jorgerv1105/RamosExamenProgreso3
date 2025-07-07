0e9929a0b308d66765746e84f669a6899e68e68f
Jorge Ramos <jorge.ramos.vasquez@udla.edu.ec>
2025-07-07 08:24:47 -0500

    Arreglo de errores de Lista de contactos

--- RamosExamenProgreso3/Views/ListaContactosPage.xaml.cs
+++ RamosExamenProgreso3/Views/ListaContactosPage.xaml.cs
@@ -1,9 +1,21 @@
+using RamosExamenProgreso3.ViewModels;
+
 namespace RamosExamenProgreso3.Views;
 
 public partial class ListadoContactosPage : ContentPage
 {
+    ListadoContactosViewModel vm;
+
     public ListadoContactosPage()
     {
         InitializeComponent();
+        vm = new ListadoContactosViewModel();
+        BindingContext = vm;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        vm.CargarContactos(); 
     }
 }
